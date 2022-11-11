1ee853cff1b8dd14e3141a4b54b8814fc91766e9
Samcodedev <obanlasamuelolakunle@gmail.com>
2022-11-11 11:58:36 +0100

    update

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
     return(
         <div className='register'>
             <h2>Fill in the form bellow to register for the EBU scholarship with Golearn.</h2>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdARHzJWENHVHhM23ioYnk8twoiP-t12yGYnUyV8VAcOt0WPQ/viewform?embedded=true" width="100%" height="500px"  frameborder="0" marginHeight="0" marginWidth="0">Loading</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdARHzJWENHVHhM23ioYnk8twoiP-t12yGYnUyV8VAcOt0WPQ/viewform?embedded=true" width="100%" height="500px"  frameborder="0" marginHeight="0" marginWidth="0" title="registeration">Loading</iframe>
         </div>
     )
 }
