6eb7aac81c65bcd7208b246fe0eacc174195c5b4
Samcodedev <obanlasamuelolakunle@gmail.com>
2022-11-14 06:10:08 +0100

    update

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -3,25 +3,25 @@ import './NavBar.css'
 import { Link } from 'react-router-dom';
 import logo from '../../img/GoLearnFull Color.png'
 import { BiMenu } from 'react-icons/bi'
-import { MdClose } from 'react-icons/md'
+// import { MdClose } from 'react-icons/md'
 
 const NavBar = () => {
 
-    document.addEventListener("click", close)
-    function close(){
-        let ull = document.getElementById("ull")
+    // document.addEventListener("click", close)
+    // function close(){
+    //     let ull = document.getElementById("ull")
 
-        if( window.innerWidth >= 1201 ){
-            ull.style.display="flex"
-        }
+    //     if( window.innerWidth >= 1201 ){
+    //         ull.style.display="flex"
+    //     }
 
-        if( ull.style.display==="block" || ull.style.display==="flex"){
-            ull.style.display="none"
-        }
-        else{
-            ull.style.display="flex"
-        }
-    }
+    //     if( ull.style.display==="block" || ull.style.display==="flex"){
+    //         ull.style.display="none"
+    //     }
+    //     else{
+    //         ull.style.display="flex"
+    //     }
+    // }
     return(
         <div className='navbar'>
             <div className="img-div">
@@ -29,13 +29,13 @@ const NavBar = () => {
             </div>
             <div className="dd">
                 <ul id='ull'>
-                <MdClose fontSize="45px" color='#ffffff' onClick={close} id='close'/>
-                    <Link to="/"><li onClick={close} >Home</li></Link>
+                {/* <MdClose fontSize="45px" color='#ffffff'  id='close'/> */}
+                    <Link to="/"><li >Home</li></Link>
                     {/* <Link to=""><li>Contact Us</li></Link> */}
-                    <Link to="/about"><li onClick={close} >About Us</li></Link>
-                    <Link to="/register"><span onClick={close} >Enrol Now</span></Link>
+                    <Link to="/about"><li >About Us</li></Link>
+                    <Link to="/register"><span >Enrol Now</span></Link>
                 </ul>
-                <BiMenu fontSize="40px" color='#ffffff' onClick={close} />
+                <BiMenu fontSize="40px" color='#ffffff' />
             </div>
         </div>
     )
