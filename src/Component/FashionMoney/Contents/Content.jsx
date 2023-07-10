06e8f0324b6de9852f888e033ee9231d1f8f015c
samcodedev <obanlasamuelolakunle@gmail.com>
2023-07-10 20:53:42 +0100

    FMWS image transitions

--- src/Component/FashionMoney/Contents/Content.jsx
+++ src/Component/FashionMoney/Contents/Content.jsx
@@ -63,43 +63,43 @@ const Content = () => {
           {card}
         </div> */}
         <p data-aos="fade-left">Many of you may already know her, but in case you don't, you can look her up on Instagram. All the explanations you will need are there. (Insert paragraph here) If you'd like to know how the conversation ended, please take a look at the picture below.</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img1} alt="_Aisy_ student" />
-          <img src={img2} alt="_Aisy_ student" />
+        <div className="img-div">
+          <img src={img1} alt="_Aisy_ student" data-aos="fade-up" />
+          <img src={img2} alt="_Aisy_ student" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">Prior to this time, I did some things: I applied for the role of Fashion illustrator to several Fashion brands.</p> 
-        <div className="img-div" data-aos="fade-up">
-          <img src={img3} alt="CV application" />
-          <img src={img4} alt="CV application" />
+        <div className="img-div">
+          <img src={img3} alt="CV application" data-aos="fade-up" />
+          <img src={img4} alt="CV application" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">But I didn’t get a response from any of them. (Insert paragraph here) I got a message from a particular big brand, and they were willing to pay high, but I felt I wanted more, so I didn’t take it. (Insert paragraph) A few months later, I got a text…</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img5} alt="Iris response from CIworkshop. IG" />
-          <img src={img6} alt="Her follow up text on WhatsApp" />
+        <div className="img-div">
+          <img src={img5} alt="Iris response from CIworkshop. IG" data-aos="fade-up" />
+          <img src={img6} alt="Her follow up text on WhatsApp" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">In case you’re wondering how this too ended, check the pictures below 😁</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img7} alt="Dotun beside claire" />
-          <img src={img8} alt="dotun teaching" />
+        <div className="img-div">
+          <img src={img7} alt="Dotun beside claire" data-aos="fade-up" />
+          <img src={img8} alt="dotun teaching" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">I know you think all of this is over, but let me tell you about one more thing. In June last year, we held the first batch of a physical illustration course for 45,000, and guess how many students we had...?</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img9} alt="" />
-          <img src={img10} alt="" />
+        <div className="img-div">
+          <img src={img9} alt="" data-aos="fade-up" />
+          <img src={img10} alt="" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">4 students!!! "Not bad," you might say, but I wasn't satisfied. So, I went back to the drawing board, made some additions, and organized another class in November. Check the picture below to see the flyer and the number of students.</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img11} alt="" />
-          <img src={img12} alt="" />
+        <div className="img-div">
+          <img src={img11} alt="" data-aos="fade-up" />
+          <img src={img12} alt="" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">Lol, yes, that's right! We had 3 students, just one less than the previous batch.</p>
         <p data-aos="fade-left">I'm not one to accept defeat. I knew there was something we were not doing right, so I did my assignment. I remember discussing with my team members in one of our meetings earlier this year, stating our goal to surpass the total number of students we had in 2022 (7 students). And guess what?</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img13} alt="" />
+        <div className="img-div">
+          <img src={img13} alt="" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">We organized another class, and this time we divided our course outline into 4 classes. And guess how many students we had...</p>
-        <div className="img-div" data-aos="fade-up">
-          <img src={img14} alt="" />
+        <div className="img-div">
+          <img src={img14} alt="" data-aos="fade-up" />
         </div>
         <p data-aos="fade-left">Yes, you counted that correctly. We had 11 students. Not only that, but we also added more features and streamlined it from 5 classes in 2022 to 4 classes.</p>
         <p data-aos="fade-left">Now, you must be wondering how we were able to achieve these results. How did I transition from applying to get gigs to having high-profile clients reaching out to me? And how did Dotun Collins Studio go from struggling to attract students to having more than double the number of students compared to the previous year, even with an increase in course fees?</p>
